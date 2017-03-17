ba5790a44a334b3c2a0db941c2256729a5c1d328
Javier Suazo <javier_suazo@live.com>
2017-03-17 16:00:59 -0600

    Fixing resolution to low

--- ios/RCTCameraManager.m
+++ ios/RCTCameraManager.m
@@ -154,7 +154,7 @@ RCT_CUSTOM_VIEW_PROPERTY(captureQuality, NSInteger, RCTCamera) {
       break;
   }
 
-  [self setCaptureQuality:qualityString];
+  [self setCaptureQuality:AVCaptureSessionPresetLow];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(aspect, NSInteger, RCTCamera) {
